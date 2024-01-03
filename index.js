95a428eec4e1cc0132a8e7ca0b610339268a834b
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-01-03 21:19:12 +0530

    modified index.js in .

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 var cors = require("cors");
 
 app.use(cors());
+
 try {
   mongoose.connect("mongodb://127.0.0.1:27017/e-comm", {
     useNewUrlParser: true,
@@ -29,8 +30,34 @@ try {
 }
 
 app.use(express.json());
+
 app.post("/register", (req, res) => {
   User.create(req.body)
-    .then((user) => res.json(user))
+    .then((user) => {
+      // Exclude the password field from the response
+      const { password, ...userWithoutPassword } = user.toObject();
+      res.json(userWithoutPassword);
+    })
     .catch((err) => res.json(err));
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email and password
+    const user = await User.findOne({ email, password }).select("-password");
+
+    if (user) {
+      // User found, send the user details (excluding password) as response
+      res.json(user);
+    } else {
+      // User not found or incorrect credentials
+      res.status(401).json({ error: "Invalid credentials" });
+    }
+  } catch (error) {
+    // Handle other errors
+    console.error("Login failed:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
